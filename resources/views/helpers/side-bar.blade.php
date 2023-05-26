6ab6b5bb2be58e2692879cef13a28ff27780a141
joelDev <procompter1@gmail.com>
2023-05-26 09:03:48 +0100

    merge test 2

--- resources/views/helpers/side-bar.blade.php
+++ resources/views/helpers/side-bar.blade.php
@@ -1,10 +1,10 @@
 @section('side-bar')
 <div class="sidenav">
     <img src="../img/avatar3.png" alt="" class="avatar"> <hr>
+    <a href="{{ url('/dashboard') }}">Menu principal</a>
     <a href="{{ url('/dashboard/actualite') }}">Actualite</a>
     <a href="{{ url('/dashboard/vote') }}">Vote simple</a>
     <a href="{{ url('/dashboard/vote/person-vote') }}">Vote de candidat</a>
     <a href="{{ url('/dashboard/sondage') }}">Sondage de vote</a>
-    <a href="{{ url('/dashboard') }}">Menu principal</a>
 </div>
 @endsection
